b221983cbe3020bdeb12e2d7065ea33a63329ace
Aiii07 <aisyahnf2899@gmail.com>
2025-09-25 16:56:58 +0700

    reactjs 1

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="text-center">
+            <h1>Welcome to My Web</h1>
+            <p>This website was built using React, Vite and Bootstrap</p>
+        </div>
+    )
+}
+
+export default Home;
