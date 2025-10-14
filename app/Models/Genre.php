626a743bc59e7c2eee2732a8b7e9b1273b8c9001
Aiii07 <aisyahnf2899@gmail.com>
2025-10-14 21:14:54 +0700

    laravel2

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -8,38 +8,7 @@ use PhpParser\Node\Expr\FuncCall;
 
 class Genre extends Model
 {
-    private $genres = [
-        [
-            'id' => 1,
-            'name' => 'Fiction',
-            'description' => 'A literary work based on the imagination and not necessarily on fact.' 
-        ],
-        [
-            'id' => 2,
-            'name' => 'Non-Fiction',
-            'description' => 'A literary work based on facts and real events.' 
-        ],
-        [
-            'id' => 3,
-            'name' => 'Fantasy',
-            'description' => 'Stories with magical or supernatural elements.' 
-        ],
-        [
-            'id' => 4,
-            'name' => 'Horor',
-            'description' => 'Fiction intended to scare or disturb the reader.' 
-        ],
-        [
-            'id' => 5,
-            'name' => 'Romance',
-            'description' => 'A genre focuses on love and romance.' 
-        ],
-    ];
-
-    public function getGenres()
-    {
-        return $this -> genres;
-    }
+    protected $table = 'genres';
 }
 
 ?>
